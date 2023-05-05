51accd3345ec9ea36a6f159ddfd0e4f008ab9cf5
MonLes <73730090+Monles@users.noreply.github.com>
2023-05-05 10:06:39 +0000

    optimization

--- src/App.js
+++ src/App.js
@@ -1,30 +1,64 @@
-import logo from './logo.svg'
-import './App.css'
-
-import { Component } from 'react'
+import './App.css';
+import { Component } from 'react';
 
 class App extends Component {
   constructor() {
-    super()
+    super();
 
     this.state = {
-      monsters: [
-        { name: 'Alice' },
-        { name: 'Boa' },
-        { name: 'Cleo' },
-        { name: 'Debra' },
-      ],
-    }
+      monsters: [],
+    };
   }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users, searchField: '' };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
+  }
+
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
   render() {
+    console.log('render');
+
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => {
-          return <h1>{monster.name}</h1>
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monster"
+          onChange={onSearchChange}
+        />
+        {filteredMonsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
         })}
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
